894804828d8eacc734f1e79cda811fccaf3468dc
CyrilSharma <sharma.cyril@gmail.com>
2023-07-15 22:42:52 -0400

    handled dangling else

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,1 @@
-mod ast;
-
-#[macro_use] extern crate lalrpop_util;
-lalrpop_mod!(pub parser); // synthesized by LALRPOP
-
 fn main() {}
-
-#[test]
-fn parser() {
-    assert!(parser::TermParser::new().parse("22").is_ok());
-    assert!(parser::TermParser::new().parse("(22)").is_ok());
-    assert!(parser::TermParser::new().parse("((((22))))").is_ok());
-    assert!(parser::TermParser::new().parse("((22)").is_err());
-}
---
+++ src/test_parser.rs
@@ -0,0 +1,9 @@
+use crate::parser::{moduleParser};
+
+#[test]
+fn test0() {
+    assert!(moduleParser::new().parse("22").is_err());
+    assert!(moduleParser::new().parse("void thing() {}").is_ok());
+    assert!(moduleParser::new().parse("int thing() { int x; }").is_ok());
+    assert!(moduleParser::new().parse("int thing() { if (1) {} }").is_ok());
+}
