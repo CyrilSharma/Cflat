fb79fbe09e7dbeb877fdf6feddeeddd6cd7cdf1e
CyrilSharma <sharma.cyril@gmail.com>
2023-07-31 00:28:19 -0400

    made it clean

--- src/cfg.rs
+++ src/cfg.rs
@@ -12,85 +12,92 @@ pub struct CFG {
 
 impl CFG {
     fn export(&self, order: &Vec<usize>) -> Vec<Statement> {
-        let mut lcount = 0;
         let mut res = Vec::<Statement>::new();
-        let mut iter = order.iter().cloned().peekable();
-        while let Some(idx) = iter.next() {
+        for idx in order.clone() {
             let n = &self.nodes[idx];
-            res.push(Statement::Label(ir::Label{id: lcount}));
+            res.push(Statement::Label(ir::Label{id: idx as u32}));
             res.extend(n.stmts.iter()
                 .take(n.stmts.len() - 1)
                 .cloned()
             );
             let last = n.stmts.last().unwrap();
-            use Statement::*;
-            match &last {
-                Jump(_) => match iter.peek() {
-                    None => res.push(Jump(ir::Label {
-                        id: n.t.unwrap() as u32
-                    })),
-                    Some(nxt) => match n.t.unwrap() == *nxt {
-                        true => res.push(Jump(ir::Label {
-                            id: n.t.unwrap() as u32
-                        })),
-                        false => (),
-                    }
-                },
-                CJump(e, _, _) => match iter.peek() {
-                    None => {
-                        res.push(CJump(
-                            Box::new(*e.clone()),
-                            ir::Label { id: n.t.unwrap() as u32 },
-                            ir::Label { id: 0 }
-                        ));
-                        res.push(Jump(ir::Label {
-                            id: n.f.unwrap() as u32
-                        }));
-                    }
-                    Some(nxt) => {
-                        if n.t.unwrap() == *nxt {
-                            res.push(CJump(
-                                Box::new(ir::Expr::UnOp(
-                                    ir::Operator::Not,
-                                    Box::new(*e.clone())
-                                )),
-                                ir::Label { id: n.t.unwrap() as u32 },
-                                ir::Label { id: 0 }
-                            ));
-                        } else if n.f.unwrap() == *nxt {
-                            res.push(CJump(
-                                Box::new(*e.clone()),
-                                ir::Label { id: n.t.unwrap() as u32 },
-                                ir::Label { id: 0 }
-                            ));
-                        } else {
-                            res.push(CJump(
-                                Box::new(*e.clone()),
-                                ir::Label { id: n.t.unwrap() as u32 },
-                                ir::Label { id: 0 }
-                            ));
-                            res.push(Jump(ir::Label {
-                                id: n.f.unwrap() as u32
-                            }));
-                        }
-                    }
-                },
-                Return(_) => res.push(last.clone()),
-                Label(_) | Seq(_) => unreachable!(),
-                _ => match iter.peek() {
-                    None => res.push(last.clone()),
-                    Some(nxt) => match n.t.unwrap() == *nxt {
-                        true => res.push(last.clone()),
-                        false => {
-                            res.push(last.clone());
-                            res.push(Jump(ir::Label {
-                                id: n.t.unwrap() as u32
-                            }));
-                        },
-                    }
+            let peek = || -> Option<usize> { 
+                match idx + 1 == order.len() {
+                    true => None,
+                    false => Some(idx + 1)
                 }
             };
-            lcount += 1;
+            let jump = || -> Vec<Statement> { match peek() {
+                None => vec![Jump(ir::Label {
+                    id: n.t.unwrap() as u32
+                })],
+                Some(nxt) => match n.t.unwrap() == nxt {
+                    true => vec![Jump(ir::Label {
+                        id: n.t.unwrap() as u32
+                    })],
+                    false => vec![],
+                }
+            }};
+            let cjump = |e: &ir::Expr| { match peek() {
+                None => vec![
+                    CJump(
+                        Box::new(e.clone()),
+                        ir::Label { id: n.t.unwrap() as u32 },
+                        ir::Label { id: 0 }
+                    ),
+                    Jump(ir::Label {
+                        id: n.f.unwrap() as u32
+                    })
+                ],
+                Some(nxt) => match (n.t.unwrap() == nxt,
+                                    n.f.unwrap() == nxt) {
+                    (true, true) => unreachable!(),
+                    (true, false) => vec![
+                        CJump(
+                        Box::new(ir::Expr::UnOp(
+                            ir::Operator::Not,
+                            Box::new(e.clone())
+                        )),
+                        ir::Label { id: n.t.unwrap() as u32 },
+                        ir::Label { id: 0 }
+                    )],
+                    (false, true) => vec![CJump(
+                        Box::new(e.clone()),
+                        ir::Label { id: n.t.unwrap() as u32 },
+                        ir::Label { id: 0 }
+                    )],
+                    (false, false) => vec![
+                        CJump(
+                            Box::new(e.clone()),
+                            ir::Label { id: n.t.unwrap() as u32 },
+                            ir::Label { id: 0 }
+                        ),
+                        Jump(ir::Label {
+                            id: n.f.unwrap() as u32
+                        })
+                    ]
+                }
+            }};
+            let notransfer = || -> Vec<Statement> { match peek() {
+                None => vec![last.clone()],
+                Some(nxt) => match n.t.unwrap() == nxt {
+                    true => vec![last.clone()],
+                    false => vec![
+                        last.clone(),
+                        Jump(ir::Label {
+                            id: n.t.unwrap() as u32
+                        })
+                    ],
+                }
+            }};
+            use Statement::*;
+            res.extend(match &last {
+                Jump(_) => jump(),
+                CJump(e, _, _) => cjump(e),
+                Return(_) => vec![last.clone()],
+                Label(_) | Seq(_) => unreachable!(),
+                _ => notransfer()
+            });
         }
         return res;
     }
